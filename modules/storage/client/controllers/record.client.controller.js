6bbb8b07e059b2c3a896c633a75dcbce9682a213
monstrasitix <injectedinferno@gmail.com>
2016-11-29 20:54:59 +0000

    Changed

--- modules/storage/client/controllers/record.client.controller.js
+++ modules/storage/client/controllers/record.client.controller.js
@@ -31,7 +31,7 @@ angular.module('storage').controller('RecordController', ['$stateParams', 'Stora
             page: 1
         };
 
-        vm.deserts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+        vm.deserts = [1, 2, 3, 4, 5, 6, 7];
 
         vm.getDesserts = function () {
             
