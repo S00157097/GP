2d8ffcbdfb3424390926174e8806bb7a3f74262e
monstrasitix <injectedinferno@gmail.com>
2016-11-29 20:17:31 +0000

    Added Dialogs

--- modules/storage/client/controllers/record.client.controller.js
+++ modules/storage/client/controllers/record.client.controller.js
@@ -1,11 +1,28 @@
 'use strict';
 
 // Create the 'chat' controller
-angular.module('storage').controller('RecordController', ['$stateParams', 'StorageService',
-    function ($stateParams, StorageService) {
+angular.module('storage').controller('RecordController', ['$stateParams', 'StorageService', '$mdDialog',
+    function ($stateParams, StorageService, $mdDialog) {
 
         var vm = this;
-        vm.categories = [];
+        vm.status = '';
 
+        vm.showAdvanced = function (ev) {
+            $mdDialog
+                .show({
+                    controller: 'DialogController',
+                    controllerAs: 'vm',
+                    templateUrl: 'modules/storage/client/templates/dialog.client.template.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true,
+                    fullscreen: false // Only for -xs, -sm breakpoints.
+                })
+                .then(function (answer) {
+                    vm.status = 'You said the information was "' + answer + '".';
+                }, function () {
+                    vm.status = 'You cancelled the dialog.';
+                });
+        };
     }
 ]);
