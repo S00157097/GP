4c4ae2caa5d9d5442770f934f3c3732b7a161579
monstrasitix <injectedinferno@gmail.com>
2017-03-21 20:04:24 +0000

    Records also show up

--- modules/dashboard/client/controllers/dashboard.controller.js
+++ modules/dashboard/client/controllers/dashboard.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 // Create the 'chat' controller
-angular.module('dashboard').controller('DashboardController', ['StorageService', 'CategoryService',
-  function (StorageService, CategoryService) {
+angular.module('dashboard').controller('DashboardController', ['StorageService', 'CategoryService', 'RecordService',
+  function (StorageService, CategoryService, RecordService) {
     var vm = this;
+    vm.records = [];
 
     StorageService.latest(2)
       .then(function (response) {
@@ -14,7 +15,7 @@ angular.module('dashboard').controller('DashboardController', ['StorageService',
           .then(function (response) {
             console.log(response);
             vm.categories = response.data;
-            
+
             vm.categories
               .map((item, index) => {
                 vm.latest.map((item2, index2) => {
@@ -22,11 +23,24 @@ angular.module('dashboard').controller('DashboardController', ['StorageService',
                     item.storageName = item2.name;
                   }
                 });
+                RecordService.list(item._id)
+                  .then(function (response) {
+                    console.log('RECORDS',response.data);
+                    vm.records[index] = response.data;
+                  });
+
                 return item;
               });
+
+            vm.categories
+
           });
       });
 
+
+      vm.convert = (obj) => {
+        return obj[Object.keys(obj)[0]]
+      };
 
   }
 ]);
